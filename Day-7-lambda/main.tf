3e0a715b4b940c8a1b5b58cc4c093aedec28f12f
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-16 16:04:05 +0530

    okay

---
+++ Day-7-lambda/main.tf
@@ -0,0 +1,78 @@
+resource "aws_iam_role" "name" {
+  name = "lambda-execution-role12"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "name" {
+  role       = aws_iam_role.name.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+resource "aws_iam_role_policy_attachment" "s3_read_only" {
+  role       = aws_iam_role.name.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_lambda_function" "name" {
+  function_name    = "my_lambda_function12"
+  role             = aws_iam_role.name.arn
+  handler          = "lambda_function12.lambda_handler"
+  runtime          = "python3.14"
+   s3_bucket        = aws_s3_bucket.name.bucket
+  s3_key           = aws_s3_bucket_object.name.key
+  source_code_hash = filebase64sha256("lambda_function12.zip")
+}
+
+#Without source_code_hash, Terraform might not detect when the code in the ZIP file has changed — meaning your Lambda might not update even after uploading a new ZIP.
+
+#This hash is a checksum that triggers a deployment.
+
+
+resource "aws_cloudwatch_event_rule" "lambda_schedule" {
+  name                = "lambda_schedule_rule"
+  description         = "Trigger Lambda every 5 minutes"
+  schedule_expression = "cron(0/5 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_schedule.name
+  target_id = "lambda-schedule-target"
+  arn       = aws_lambda_function.name.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.name.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_schedule.arn
+}
+resource "aws_s3_bucket" "name" {
+  bucket = "rojinayakkkksuppp1234boss"
+  tags = {
+    Name = "LambdaCodeBucket11111"
+  }
+  
+}
+resource "aws_s3_bucket_object" "name" {
+  bucket = aws_s3_bucket.name.bucket
+  key    = "lambda1/lambda_function12.zip"
+  source = "lambda_function12.zip"
+  etag   = filemd5("lambda_function12.zip")
+  
+
+}
+
+ 
+
+
