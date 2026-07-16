49204dca1e3ddd3fce00afe1eb218173507673b6
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-16 15:16:20 +0530

    ok

--- Day-6-lambda/main.tf
+++ Day-6-lambda/main.tf
@@ -1,42 +1,54 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda-execution-role"
-
+resource "aws_iam_role" "name" {
+  name = "lambda-execution-role12"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
   })
 }
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_role.name
+
+resource "aws_iam_role_policy_attachment" "name" {
+  role       = aws_iam_role.name.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-resource "aws_lambda_function" "my_lambda" {
 
-  function_name = "my-lambda"
-
-  filename         = "lambda.zip"
-  source_code_hash = filebase64sha256("lambda.zip")
-
-  role    = aws_iam_role.lambda_role.arn
-  handler = "lambda_function.lambda_handler"
-
-  runtime = "python3.12"
-
-  timeout      = 30
-  memory_size  = 128
-
-  tags = {
-    Name = "MyLambda"
-  }
+resource "aws_lambda_function" "name" {
+  function_name    = "my_lambda_function12"
+  role             = aws_iam_role.name.arn
+  handler          = "lambda_function12.lambda_handler"
+  runtime          = "python3.9"
+  filename         = "lambda_function12.zip"
+  source_code_hash = filebase64sha256("lambda_function12.zip")
 }
-output "lambda_arn" {
-  value = aws_lambda_function.my_lambda.arn
+
+#Without source_code_hash, Terraform might not detect when the code in the ZIP file has changed — meaning your Lambda might not update even after uploading a new ZIP.
+
+#This hash is a checksum that triggers a deployment.
+
+
+resource "aws_cloudwatch_event_rule" "lambda_schedule" {
+  name                = "lambda_schedule_rule"
+  description         = "Trigger Lambda every 5 minutes"
+  schedule_expression = "cron(0/5 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.lambda_schedule.name
+  target_id = "lambda-schedule-target"
+  arn       = aws_lambda_function.name.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.name.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.lambda_schedule.arn
 }
