38fb7841479bd786ea4fecde83b9afcb1dc34276
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-17 14:02:29 +0530

    kk

---
+++ Day-8-Modules/variables.tf
@@ -0,0 +1,15 @@
+
+variable "ami_id" {
+    description = "The AMI ID to use for the instance"
+    default = "" # Replace with a valid AMI ID for your region
+}
+variable "instance_type" {
+    default = ""
+}
+variable "instance_name" {
+    default = ""
+}
+  
+
+
+
