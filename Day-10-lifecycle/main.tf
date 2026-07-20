b43fe0c1c102a43d6fc18692da97c0877e390477
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-20 16:01:39 +0530

    kk

---
+++ Day-10-lifecycle/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "name" {
+    ami = "ami-01edba92f9036f76e1"
+    instance_type = "t2.medium"
+    tags = {
+        Name = "server-1"
+          }
+    
+
+    lifecycle {
+      create_before_destroy = true  #first create and the destroy
+    }
+    # lifecycle {
+    #   ignore_changes = [ tags ]
+    # }
+    # lifecycle {
+    #   prevent_destroy = true
+    # }
+}
