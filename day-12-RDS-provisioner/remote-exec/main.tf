68675e22a3dc6d425a225eca5a494cec4d456893
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-23 17:07:47 +0530

    rds

--- day-12-RDS-provisioner/remote-exec/main.tf
+++ day-12-RDS-provisioner/remote-exec/main.tf
@@ -1,51 +1,66 @@
-resource "aws_key_pair" "example" {
-  key_name   = "task"
-  public_key = file("~/.ssh/id_ed25519.pub")
-}
-# Example EC2 instance (replace with yours if already existing)
-resource "aws_instance" "sql_runner" {
-  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.example.key_name             # Replace with your key pair name
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "SQL Runner"
-  }
+provider "aws" {
+  
 }
 
-# Deploy SQL remotely using null_resource + remote-exec
-resource "null_resource" "remote_sql_exec" {
-  depends_on = [aws_db_instance.mysql_rds, aws_instance.sql_runner]
+resource "aws_key_pair" "name" {
+    key_name = "monitor"
+    public_key = file("~/.ssh/id_ed25519.pub")
+  
+}
 
+
+resource "aws_instance" "name" {
+    ami="ami-07860a2d7eb515d9a"
+    instance_type = "t3.micro"
+    key_name = aws_key_pair.name.key_name
+    associate_public_ip_address = true
+   tags={
+    Name="rds"
+   }
+}
+
+ #Create the RDS instance
+
+resource "aws_db_instance" "mysql_rds" {
+  identifier              = "my-mysql-dbbbbb"
+  engine                  = "mysql"
+  instance_class          = "db.t3.micro"
+  username                = "admin"
+  password                = "Password12"
+  db_name                 = "dev"
+  allocated_storage       = 20
+  skip_final_snapshot     = true
+  publicly_accessible     = true
+}
+
+#---------------null-resources--------#
+
+resource "null_resource" "name" {
+  depends_on = [aws_db_instance.mysql_rds, aws_instance.name]
+
+  #SSH Connection to EC2
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("~/.ssh/id_ed25519")   # Replace with your PEM file path
-    host        = aws_instance.sql_runner.public_ip
+    host        = aws_instance.name.public_ip
+    private_key = file("~/.ssh/id_ed25519")
   }
 
+  # Copy the SQL file from your local machine to EC2
   provisioner "file" {
-    source      = "init.sql"
-    destination = "/tmp/init.sql"
+    source      = "init.sql"                 # path on your local machine
+    destination = "/home/ec2-user/init.sql"  # path on EC2
   }
+
+  # 3Run MySQL command remotely inside EC2
 
   provisioner "remote-exec" {
     inline = [
-      "yum install mariadb105 server -y",
-      #"mysql -h ${aws_db_instance.mysql_rds.address} -u ${jsondecode(aws_secretsmanager_secret_version.rds_secret_value.secret_string)["username"]} -p${jsondecode(aws_secretsmanager_secret_version.rds_secret_value.secret_string)["password"]} < /tmp/init.sql"
-      "mysql -h ${aws_db_instance.mysql_rds.address} -u admin -pPassword123! < /tmp/init.sql"
+      # Install MySQL client if missing
+      "sudo yum install -y mariadb105-server",
+
+      # Run the SQL file on your RDS instance
+      "mysql -h ${aws_db_instance.mysql_rds.address} -u admin -pPassword12 dev < /home/ec2-user/init.sql"
     ]
   }
-
-  triggers = {
-    always_run = timestamp() #trigger every time apply 
-  }
 }
-
-
-
-
-# ADD RDS creation script only accessbale interanlly si disable public access 
-# Remote provisioner server also should create insame vpc 
-# enable secrets fro secret manager and call secrets into RDS for this process vpc endpoint is require or nat gateway is required to access secrets to rds internall as secremanger is not in side VPC sefrvice 
