2d315624625dff6c527120994e7c5803b571a6ad
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-07-23 14:42:23 +0530

    hh

---
+++ Day-13-count-foreach/count/variables.tf
@@ -0,0 +1,5 @@
+variable "tags" {
+    type = list(string)
+    default = [ "server2","server5"]
+  
+}
---
+++ Day-13-count-foreach/for_each/variable.tf
@@ -0,0 +1,5 @@
+variable "tags" {
+    type = list(string)
+    default = [ "pratu","hima"]  #in for each server name are key--as mentioned in main file
+  
+}
