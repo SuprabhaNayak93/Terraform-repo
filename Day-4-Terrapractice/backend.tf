4d742a07964d5ee932445c8a4a4c56403ac29a79
SuprabhaNayak93 <rojirenu5@gmail.com>
2026-09-05 12:02:41 +0530

    Update backend.tf

--- Day-4-Terrapractice/backend.tf
+++ Day-4-Terrapractice/backend.tf
@@ -1,8 +1,18 @@
+#terraform {
+ # backend "s3" {
+   # bucket = "terraformbuckets3supppbbbnayak"
+    #key    = "terraform.tfstate"
+    #region = "us-east-1"
+    #use_lockfile = true
+  #}
+#}
 terraform {
   backend "s3" {
-    bucket = "terraformbuckets3supppbbbnayak"
+    bucket = "dev-test-prod-nit"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true
+    #use_lockfile = true ##supports terrafrom latest version >=1.10
+   dynamodb_table = "terraform-state-locking"  #if terrafrom version <1.10 use below code
+    
   }
 }
